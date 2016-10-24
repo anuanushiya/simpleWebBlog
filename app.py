034fbfe9c3682553acbe4f336b028fc0fa0c4821
AnushiyadeviMagudeeswaran <anushiya45@gmail.com>
2016-10-24 17:28:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def create_new_post(blog_id):
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'some secret key'
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=6000)
